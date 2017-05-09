0139163fd3166c3f16ae652e74fe91dd8d8d744c
Dillon "Ignotis" Morse <dillon.arthur.morse@gmail.com>
2017-05-09 14:57:25 -0400

    Create a non-functional GUI.
    The GUI has a menu bar with File->Save and File->Open and a text area.

--- ASD-Senior-Project/src/me/dillonmorse/asdseniorproject/ASDSeniorProject.java
+++ ASD-Senior-Project/src/me/dillonmorse/asdseniorproject/ASDSeniorProject.java
@@ -1,19 +1,38 @@
 package me.dillonmorse.asdseniorproject;
 
+import java.awt.Dimension;
+
 import javax.swing.JFrame;
-import javax.swing.JLabel;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
 import javax.swing.SwingUtilities;
 
 public class ASDSeniorProject implements Runnable {
 	
 	public void run() {
 		// Create the window
-		JFrame frame = new JFrame("HelloWorldSwing");
+		JFrame frame = new JFrame("ASD Senior Project");
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
-		// Temporary "Hello, world!" label
-		JLabel label = new JLabel("Hello, world!");
-		frame.getContentPane().add(label);
+		// Menu bar for saving and opening
+		// TODO: saving and opening functions
+		JMenuBar menuBar = new JMenuBar();
+		JMenu fileMenu = new JMenu("File");
+		JMenuItem saveMenuItem = new JMenuItem("Save");
+		fileMenu.add(saveMenuItem);
+		JMenuItem openMenuItem = new JMenuItem("Open");
+		fileMenu.add(openMenuItem);
+		menuBar.add(fileMenu);
+		frame.setJMenuBar(menuBar);
+		
+		// Scrolling text editing field
+		JTextArea textArea = new JTextArea();
+		textArea.setPreferredSize(new Dimension(640, 480));
+		JScrollPane scrollPane = new JScrollPane(textArea);
+		frame.getContentPane().add(scrollPane);
 		
 		// Display the window
 		frame.pack();
