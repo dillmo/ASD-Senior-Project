d561ec109aa643130ad704acab1351c3c5900b69
Dillon "Ignotis" Morse <dillon.arthur.morse@gmail.com>
2017-05-14 16:37:49 -0400

    Initial save action logic

---
+++ ASD-Senior-Project/src/me/dillonmorse/asdseniorproject/actions/SaveAction.java
@@ -0,0 +1,20 @@
+package me.dillonmorse.asdseniorproject.actions;
+
+import java.awt.event.ActionEvent;
+
+import javax.swing.AbstractAction;
+
+public class SaveAction extends AbstractAction {
+
+	private static final long serialVersionUID = 1L;
+
+	public SaveAction(String name) {
+		super(name);
+	}
+
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
