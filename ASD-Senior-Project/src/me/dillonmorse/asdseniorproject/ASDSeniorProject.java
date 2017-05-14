d293c822cd543f4d072b536728d9350cc8efd2bf
Dillon <Dillon@DESKTOP-CIP3U78.lan>
2017-05-14 19:30:58 -0400

    Working file saving implementation sans encryption

--- ASD-Senior-Project/src/me/dillonmorse/asdseniorproject/ASDSeniorProject.java
+++ ASD-Senior-Project/src/me/dillonmorse/asdseniorproject/ASDSeniorProject.java
@@ -1,41 +1,85 @@
 package me.dillonmorse.asdseniorproject;
 
+import java.awt.BorderLayout;
 import java.awt.Dimension;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
 
+import javax.swing.JFileChooser;
 import javax.swing.JFrame;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
+import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
 import javax.swing.SwingUtilities;
 
-public class ASDSeniorProject implements Runnable {
+public class ASDSeniorProject extends JPanel implements Runnable, ActionListener {
+	
+	private static final long serialVersionUID = 1L;
+	private JFileChooser fileChooser;
+	private JMenuItem saveMenuItem, openMenuItem;
+	private JTextArea textArea;
+	
+	public ASDSeniorProject() {
+		super(new BorderLayout());
+		
+		// Set up the file chooser
+		fileChooser = new JFileChooser();
+		
+		// Menu bar for saving and opening
+		JMenuBar menuBar = new JMenuBar();
+		JMenu fileMenu = new JMenu("File");
+		
+		// Saving
+		saveMenuItem = new JMenuItem("Save");
+		saveMenuItem.addActionListener(this);
+		fileMenu.add(saveMenuItem);
+		
+		// Opening
+		openMenuItem = new JMenuItem("Open");
+		openMenuItem.addActionListener(this);
+		fileMenu.add(openMenuItem);
+		
+		menuBar.add(fileMenu);
+		this.add(menuBar, BorderLayout.NORTH);
+		
+		// Scrolling text editing field
+		textArea = new JTextArea();
+		JScrollPane scrollPane = new JScrollPane(textArea);
+		this.add(scrollPane, BorderLayout.CENTER);
+	}
 	
 	public void run() {
 		// Create the window
 		JFrame frame = new JFrame("ASD Senior Project");
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
-		// Menu bar for saving and opening
-		// TODO: saving and opening functions
-		JMenuBar menuBar = new JMenuBar();
-		JMenu fileMenu = new JMenu("File");
-		JMenuItem saveMenuItem = new JMenuItem("Save");
-		fileMenu.add(saveMenuItem);
-		JMenuItem openMenuItem = new JMenuItem("Open");
-		fileMenu.add(openMenuItem);
-		menuBar.add(fileMenu);
-		frame.setJMenuBar(menuBar);
-		
-		// Scrolling text editing field
-		JTextArea textArea = new JTextArea();
-		JScrollPane scrollPane = new JScrollPane(textArea);
-		frame.getContentPane().add(scrollPane);
+		// Add content to the window
+		frame.add(new ASDSeniorProject());
 		
 		// Display the window
 		frame.setSize(new Dimension(640, 480));
 		frame.setVisible(true);
+	}
+
+	public void actionPerformed(ActionEvent e) {
+		if (e.getSource() == saveMenuItem) {
+			int returnVal = fileChooser.showSaveDialog(ASDSeniorProject.this);
+			
+			if (returnVal == JFileChooser.APPROVE_OPTION) {
+				File file = fileChooser.getSelectedFile();
+				
+				try {
+					byte[] bytes = ASDSeniorProject.this.textArea.getText().getBytes();
+					Files.write(file.toPath(), bytes);
+				} catch (IOException x) {}
+			}
+		}
 	}
 
 	public static void main(String[] args) {
