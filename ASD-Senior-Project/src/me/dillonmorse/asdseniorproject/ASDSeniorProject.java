c32ca218ec4b9a5c07b2e94ea18cfbd89d0efb82
Dillon <Dillon@DESKTOP-CIP3U78.lan>
2017-05-16 09:06:45 -0400

    Encryption and decryption logic

--- ASD-Senior-Project/src/me/dillonmorse/asdseniorproject/ASDSeniorProject.java
+++ ASD-Senior-Project/src/me/dillonmorse/asdseniorproject/ASDSeniorProject.java
@@ -7,12 +7,25 @@ import java.awt.event.ActionListener;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
+import java.security.InvalidAlgorithmParameterException;
+import java.security.InvalidKeyException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Arrays;
+import java.util.Random;
 
+import javax.crypto.BadPaddingException;
+import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
+import javax.crypto.spec.IvParameterSpec;
+import javax.crypto.spec.SecretKeySpec;
 import javax.swing.JFileChooser;
 import javax.swing.JFrame;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
@@ -21,9 +34,11 @@ import javax.swing.SwingUtilities;
 public class ASDSeniorProject extends JPanel implements Runnable, ActionListener {
 	
 	private static final long serialVersionUID = 1L;
+	private static JFrame frame;
 	private JFileChooser fileChooser;
 	private JMenuItem saveMenuItem, openMenuItem;
 	private JTextArea textArea;
+	private Random rng;
 	
 	public ASDSeniorProject() {
 		super(new BorderLayout());
@@ -56,7 +71,7 @@ public class ASDSeniorProject extends JPanel implements Runnable, ActionListener
 	
 	public void run() {
 		// Create the window
-		JFrame frame = new JFrame("ASD Senior Project");
+		frame = new JFrame("ASD Senior Project");
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
 		// Add content to the window
@@ -65,6 +80,60 @@ public class ASDSeniorProject extends JPanel implements Runnable, ActionListener
 		// Display the window
 		frame.setSize(new Dimension(640, 480));
 		frame.setVisible(true);
+	}
+	
+	// Gets a password from the user
+	private static byte[] getPassword() {
+		// Ask the user for a password
+		PasswordDialog dialog = new PasswordDialog(frame);
+		dialog.open();
+		byte[] password = new String(dialog.passwordField.getPassword()).getBytes();
+		dialog.destruct();
+		
+		return password;
+	}
+	
+	// Concatenates two byte arrays of any length
+	private static byte[] byteArrayConcat(byte[] array1, byte[] array2) {
+		byte[] concatenatedArray = new byte[array1.length + array2.length];
+		System.arraycopy(array1, 0, concatenatedArray, 0, array1.length);
+		System.arraycopy(array2, 0, concatenatedArray, array1.length, array2.length);
+		
+		return concatenatedArray;
+	}
+	
+	// Generates a 128-bit secret using salted MD5
+	private static byte[] genSaltedMD5(byte[] password, byte[] salt) throws NoSuchAlgorithmException {
+		// Salt the password
+		byte[] saltedPassword = byteArrayConcat(password, salt);
+		
+		// Hash the salted password with MD5 for a 128-bit secret
+		MessageDigest messageDigest = MessageDigest.getInstance("MD5");
+		byte[] secret = messageDigest.digest(saltedPassword);
+		
+		return secret;
+	}
+	
+	// Generates a secret key from a password and a salt
+	private static SecretKeySpec genSecretKey(byte[] password, byte[] salt) throws NoSuchAlgorithmException {
+		// Generate a 128-bit secret using salted MD5
+		byte[] secret = genSaltedMD5(password, salt);
+		
+		// Generate a secret key using the secret
+		SecretKeySpec secretKey = new SecretKeySpec(secret, "AES");
+		
+		return secretKey;
+	}
+	
+	// Generates an initialization vector from a password and a salt
+	private static IvParameterSpec genIV(byte[] password, byte[] salt) throws NoSuchAlgorithmException {
+		// Generate a 128-bit secret using salted MD5
+		byte[] secret = genSaltedMD5(password, salt);
+		
+		// Generate an initialization vector using the secret
+		IvParameterSpec iv = new IvParameterSpec(secret);
+		
+		return iv;
 	}
 
 	public void actionPerformed(ActionEvent e) {
@@ -77,9 +146,45 @@ public class ASDSeniorProject extends JPanel implements Runnable, ActionListener
 				
 				// Save the file
 				try {
-					byte[] bytes = ASDSeniorProject.this.textArea.getText().getBytes();
-					Files.write(file.toPath(), bytes);
-				} catch (IOException x) {}
+					// Generate a secret key from a user-provided password and a random 256-bit salt
+					rng = new Random();
+					byte[] keySalt = new byte[32];
+					rng.nextBytes(keySalt);
+					byte[] password = getPassword();
+					SecretKeySpec secretKey = genSecretKey(password, keySalt);
+					
+					// Generate an initialization vector from the same password and another salt
+					byte[] ivSalt = new byte[32];
+					rng.nextBytes(ivSalt);
+					IvParameterSpec iv = genIV(password, ivSalt);
+					
+					// Initialize a cipher for encryption
+					Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
+					cipher.init(Cipher.ENCRYPT_MODE, secretKey, iv);
+					
+					// Encrypt the text
+					byte[] encryptedText = cipher.doFinal(ASDSeniorProject.this.textArea.getText().getBytes());
+					
+					// Append both salts to the text
+					byte[] finalText = byteArrayConcat(byteArrayConcat(encryptedText, keySalt), ivSalt);
+					
+					// Write the encrypted text to the file
+					Files.write(file.toPath(), finalText);
+				} catch (IOException e1) {
+					e1.printStackTrace();
+				} catch (NoSuchAlgorithmException e1) {
+					e1.printStackTrace();
+				} catch (NoSuchPaddingException e1) {
+					e1.printStackTrace();
+				} catch (InvalidKeyException e1) {
+					e1.printStackTrace();
+				} catch (IllegalBlockSizeException e1) {
+					e1.printStackTrace();
+				} catch (BadPaddingException e1) {
+					e1.printStackTrace();
+				} catch (InvalidAlgorithmParameterException e1) {
+					e1.printStackTrace();
+				}
 			}
 		// Opening files
 		} else if (e.getSource() == openMenuItem) {
@@ -90,9 +195,44 @@ public class ASDSeniorProject extends JPanel implements Runnable, ActionListener
 				
 				// Open the file
 				try {
-					String text = new String(Files.readAllBytes(file.toPath()));
+					// Read the encrypted file
+					byte[] fileBytes = Files.readAllBytes(file.toPath());
+					byte[] encryptedText = Arrays.copyOfRange(fileBytes, 0, fileBytes.length - 64);
+					byte[] keySalt = Arrays.copyOfRange(fileBytes, fileBytes.length - 64, fileBytes.length - 32);
+					byte[] ivSalt = Arrays.copyOfRange(fileBytes, fileBytes.length - 32, fileBytes.length);
+					
+					// Generate a secret key from a user-provided password and the key salt
+					byte[] password = getPassword();
+					SecretKeySpec secretKey = genSecretKey(password, keySalt);
+					
+					// Generated an initialization vector using the password and the IV salt
+					IvParameterSpec iv = genIV(password, ivSalt);
+					
+					// Initialize a cipher for decryption
+					Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
+					cipher.init(Cipher.DECRYPT_MODE, secretKey, iv);
+					
+					// Decrypt the text
+					String text = new String(cipher.doFinal(encryptedText));
+					
+					// Display the decrypted text
 					ASDSeniorProject.this.textArea.setText(text);
-				} catch (IOException x) {}
+				} catch (IOException e1) {
+					e1.printStackTrace();
+				} catch (NoSuchAlgorithmException e1) {
+					e1.printStackTrace();
+				} catch (NoSuchPaddingException e1) {
+					e1.printStackTrace();
+				} catch (InvalidKeyException e1) {
+					e1.printStackTrace();
+				} catch (IllegalBlockSizeException e1) {
+					e1.printStackTrace();
+				} catch (BadPaddingException e1) {
+					JOptionPane.showMessageDialog(ASDSeniorProject.this, "Incorrect password", "Error", JOptionPane.ERROR_MESSAGE);
+					e1.printStackTrace();
+				} catch (InvalidAlgorithmParameterException e1) {
+					e1.printStackTrace();
+				}
 			}
 		}
 	}
---
+++ ASD-Senior-Project/src/me/dillonmorse/asdseniorproject/PasswordDialog.java
@@ -0,0 +1,44 @@
+package me.dillonmorse.asdseniorproject;
+
+import java.awt.BorderLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JDialog;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPasswordField;
+
+public class PasswordDialog extends JDialog implements ActionListener {
+
+	private static final long serialVersionUID = 1L;
+	public JPasswordField passwordField;
+	
+	public PasswordDialog(JFrame owner) {
+		super(owner, true);
+		
+		this.setLayout(new BorderLayout());
+		JLabel label = new JLabel("Password: ");
+		this.getContentPane().add(label, BorderLayout.WEST);
+		passwordField = new JPasswordField(10);
+		this.getContentPane().add(passwordField, BorderLayout.CENTER);
+		JButton button = new JButton("OK");
+		button.addActionListener(this);
+		this.getContentPane().add(button, BorderLayout.EAST);
+		this.pack();
+	}
+
+	public void actionPerformed(ActionEvent arg0) {
+		this.setVisible(false);
+	}
+	
+	public void open() {
+		this.setVisible(true);
+	}
+	
+	public void destruct() {
+		this.dispose();
+	}
+
+}
